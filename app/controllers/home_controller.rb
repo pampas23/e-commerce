6688a5d74539abbe473a6c7e65fcfbe4aeac8622
Pampas <HaoYi@chenhaoyide-MacBook-Air.local>
2017-01-09 11:28:55 +0800

    feat(efficacy) fix home N+1 problem

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
 	def index
 		@categories = Category.where(enable:true).all
-		@products = Product.order('id desc').page(params[:page])
+		@products = Product.order('id desc').includes(:category).page(params[:page])
 	end
 end
